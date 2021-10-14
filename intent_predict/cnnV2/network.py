fad20dddcb2774f021925c479f17ca85d9a36985
paperfront <mattlacayo@berkeley.edu>
2021-10-14 15:41:44 -0700

    Created network skeleton architecture

---
+++ intent_predict/cnnV2/network.py
@@ -0,0 +1,95 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+from torchvision import models
+
+from torchvision.models import inception
+
+
+class SimpleCNN(nn.Module):
+    """
+    Simple CNN.
+    """
+    def __init__(self, dropout_p = 0.2):
+        """
+        Instantiate the model
+        """
+        super(SimpleCNN, self).__init__()
+
+        self.img_layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=6, kernel_size=7, stride=2, padding=3),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(num_features=6)
+        )
+
+        self.img_layer2 = nn.Sequential(
+            nn.Conv2d(in_channels=6, out_channels=16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(num_features=16)
+        )
+
+        self.img_layer3 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=3, kernel_size=1, stride=1, padding=0),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(num_features=3)
+        )
+        
+        self.flatten_layer = nn.Sequential(
+            nn.Flatten(),
+        )
+        
+        IMG_LAYER_OUTPUT_SIZE = 120000
+        NON_SPATIAL_FEATURE_SIZE = 5
+        
+        
+        self.linear_layer1 = nn.Sequential(
+            nn.Linear(IMG_LAYER_OUTPUT_SIZE + NON_SPATIAL_FEATURE_SIZE, 1000),
+            nn.ReLU(inplace=True),
+        )
+        
+        self.linear_layer2 = nn.Sequential(
+            nn.Linear(1000, 100),
+            nn.ReLU(inplace=True),
+        )
+        
+        self.linear_layer3 = nn.Sequential(
+            nn.Linear(100, 1),
+            nn.ReLU(inplace=True),
+        )
+
+        self.dropout = nn.Dropout(p=dropout_p)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, img_feature, non_spatial_feature):
+        """
+        forward method
+        """
+        x = self.img_layer1(img_feature)
+        x = self.dropout(x)
+
+        x = self.img_layer2(x)
+        x = self.dropout(x)
+
+        x = self.img_layer3(x)
+        x = self.dropout(x)
+        
+        x = self.flatten_layer(x)
+        x = self.dropout(x)
+        
+        
+        non_spatial_feature = self.flatten_layer(non_spatial_feature)
+        
+        x = torch.cat([x, non_spatial_feature], 1)
+        x = self.linear_layer1(x)
+        x = self.dropout(x)
+
+        x = self.linear_layer2(x)
+        x = self.dropout(x)
+
+        x = self.linear_layer3(x)
+        x = self.dropout(x)
+
+        x = self.sigmoid(x)
+
+        return x
--- intent_predict/cnnV2/utils.py
+++ intent_predict/cnnV2/utils.py
@@ -5,14 +5,17 @@ class CNNDataset(Dataset):
     """
     Dataset containing the instance-centric crop image, the non-spatial features, and the label
     """
-    def __init__(self, file_path, transform=None, target_transform=None):
+    def __init__(self, file_path, input_transform=None, target_transform=None):
         """
         Instantiate the dataset
         """
+        #self.image_features = (np.load('%s_image_feature.npy' % file_path) /
+        #255).astype(np.single)
         self.image_features = np.load('%s_image_feature.npy' % file_path)
+        #self.image_features = self.image_features.transpose(0, 3, 1, 2)
         self.non_spatial_features = np.load('%s_non_spatial_feature.npy' % file_path)
         self.all_labels = np.load('%s_label.npy' % file_path)
-        self.transform = transform
+        self.input_transform = input_transform
         self.target_transform = target_transform
 
     def __len__(self):
@@ -28,9 +31,9 @@ class CNNDataset(Dataset):
         img_feature = self.image_features[idx]
         non_spatial_feature = self.non_spatial_features[idx]
         label = self.all_labels[idx]
-        if self.transform:
-            img_feature = self.transform(img_feature)
-            non_spatial_feature = self.transform(non_spatial_feature)
+        if self.input_transform:
+            img_feature = self.input_transform(img_feature)
+            non_spatial_feature = non_spatial_feature.astype(np.single)
         if self.target_transform:
             label = self.target_transform(label)
             
