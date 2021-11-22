8abfcc1eff9ab01ff33d33b5ae127704251d5e7c
paperfront <mattlacayo@berkeley.edu>
2021-11-22 02:50:00 -0800

    Update cross_validate.py

--- intent_predict/cnnV2/cross_validate.py
+++ intent_predict/cnnV2/cross_validate.py
@@ -31,122 +31,125 @@ def main():
 
     results = {}
     num_folds = 5
-    num_epochs= 25
-    learning_rates = [1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+    num_epochs= 15
+    learning_rates = [1e-4, 1e-3, 1e-2, 1e-1]
     kfold = KFold(n_splits=num_folds, shuffle=True)
-    
-    for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
-        # Print
-        print(f'FOLD {fold}')
-        print('--------------------------------')
-        
-        # Sample elements randomly from a given list of ids, no replacement.
-        train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
-        test_subsampler = torch.utils.data.SubsetRandomSampler(test_ids)
-        
-        # Define data loaders for training and testing data in this fold
-        trainloader = torch.utils.data.DataLoader(
-                        dataset, 
-                        batch_size=64, sampler=train_subsampler, num_workers=10)
-        testloader = torch.utils.data.DataLoader(
-                        dataset,
-                        batch_size=64, sampler=test_subsampler, num_workers=10)
-        
-        # Init the neural network
-        network = SimpleCNN().cuda()
-        
-        # Initialize optimizer
-        optimizer = torch.optim.AdamW(network.parameters(), lr=learning_rates[fold])
-        
-        loss_fn = torch.nn.BCEWithLogitsLoss().cuda()
-        
-        # Run the training loop for defined number of epochs
-        for epoch in range(0, num_epochs):
 
-            # Print epoch
-            print(f'Starting epoch {epoch+1}')
+    for lr_idx, learning_rate in enumerate(learning_rates):
+        print(f"STARTING CV FOR LEARNING RATE = {learning_rate}:")
+        print("=================================\n\n")
+        for fold, (train_ids, test_ids) in enumerate(kfold.split(dataset)):
+            # Print
+            print(f'FOLD {fold}')
+            print('--------------------------------')
+            
+            # Sample elements randomly from a given list of ids, no replacement.
+            train_subsampler = torch.utils.data.SubsetRandomSampler(train_ids)
+            test_subsampler = torch.utils.data.SubsetRandomSampler(test_ids)
+            
+            # Define data loaders for training and testing data in this fold
+            trainloader = torch.utils.data.DataLoader(
+                            dataset, 
+                            batch_size=64, sampler=train_subsampler, num_workers=10)
+            testloader = torch.utils.data.DataLoader(
+                            dataset,
+                            batch_size=64, sampler=test_subsampler, num_workers=10)
+            
+            # Init the neural network
+            network = SimpleCNN().cuda()
+            
+            # Initialize optimizer
+            optimizer = torch.optim.AdamW(network.parameters(), lr=learning_rate)
+            
+            loss_fn = torch.nn.BCEWithLogitsLoss().cuda()
+            
+            # Run the training loop for defined number of epochs
+            for epoch in range(0, num_epochs):
 
-            # Set current loss value
-            current_loss = 0.0
+                # Print epoch
+                print(f'Starting epoch {epoch+1}')
 
-            # Iterate over the DataLoader for training data
-            for i, data in enumerate(trainloader, 0):
-                
-                img_feature, non_spatial_feature, labels = data
-                img_feature = img_feature.cuda()
-                non_spatial_feature = non_spatial_feature.cuda()
-                labels = labels.cuda().unsqueeze(1)
-                
-                # Zero the gradients
-                optimizer.zero_grad()
-                
-                # Perform forward pass
-                preds = network(img_feature, non_spatial_feature)
-                
-                # Compute loss
-                loss = loss_fn(preds, labels)
-                
-                # Perform backward pass
-                loss.backward()
-                
-                # Perform optimization
-                optimizer.step()
-                
-                # Print statistics
-                current_loss += loss.item()
-                if i % 50 == 49:
-                    print('Loss after mini-batch %5d: %.3f' %
-                        (i + 1, current_loss / 50))
-                    current_loss = 0.0
+                # Set current loss value
+                current_loss = 0.0
+
+                # Iterate over the DataLoader for training data
+                for i, data in enumerate(trainloader, 0):
                     
-        # Process is complete.
-        print('Training process has finished. Saving trained model.')
+                    img_feature, non_spatial_feature, labels = data
+                    img_feature = img_feature.cuda()
+                    non_spatial_feature = non_spatial_feature.cuda()
+                    labels = labels.cuda().unsqueeze(1)
+                    
+                    # Zero the gradients
+                    optimizer.zero_grad()
+                    
+                    # Perform forward pass
+                    preds = network(img_feature, non_spatial_feature)
+                    
+                    # Compute loss
+                    loss = loss_fn(preds, labels)
+                    
+                    # Perform backward pass
+                    loss.backward()
+                    
+                    # Perform optimization
+                    optimizer.step()
+                    
+                    # Print statistics
+                    current_loss += loss.item()
+                    if i % 50 == 49:
+                        print('Loss after mini-batch %5d: %.3f' %
+                            (i + 1, current_loss / 50))
+                        current_loss = 0.0
+                        
+            # Process is complete.
+            print('Training process has finished. Saving trained model.')
 
-        # Print about testing
-        print('Starting testing')
-        
-        # Saving the model
-        save_path = f'models//model-fold-{fold}.pth'
-        torch.save(network.state_dict(), save_path)
+            # Print about testing
+            print('Starting testing')
+            
+            # Saving the model
+            save_path = f'models//model-fold-{fold}-lr-{lr_idx}.pth'
+            torch.save(network.state_dict(), save_path)
 
-        # Evaluationfor this fold
-        correct, total = 0, 0
-        with torch.no_grad():
-            # Iterate over the test data and generate predictions
-            for i, data in enumerate(testloader, 0):
+            # Evaluationfor this fold
+            correct, total = 0, 0
+            with torch.no_grad():
+                # Iterate over the test data and generate predictions
+                for i, data in enumerate(testloader, 0):
 
-                img_feature, non_spatial_feature, labels = data
-                img_feature = img_feature.cuda()
-                non_spatial_feature = non_spatial_feature.cuda()
-                labels = labels.cuda().unsqueeze(1)
-                
-                # Zero the gradients
-                optimizer.zero_grad()
-                
-                # Perform forward pass
-                preds = network(img_feature, non_spatial_feature)
-                
-                # Compute loss
-                loss = loss_fn(preds, labels)
+                    img_feature, non_spatial_feature, labels = data
+                    img_feature = img_feature.cuda()
+                    non_spatial_feature = non_spatial_feature.cuda()
+                    labels = labels.cuda().unsqueeze(1)
+                    
+                    # Zero the gradients
+                    optimizer.zero_grad()
+                    
+                    # Perform forward pass
+                    preds = network(img_feature, non_spatial_feature)
+                    
+                    # Compute loss
+                    loss = loss_fn(preds, labels)
 
-                # Set total and correct
-                _, predicted = torch.max(preds.data, 1)
-                total += labels.size(0)
-                correct += (predicted == labels).sum().item()
+                    # Set total and correct
+                    _, predicted = torch.max(preds.data, 1)
+                    total += labels.size(0)
+                    correct += (predicted == labels).sum().item()
 
-        # Print accuracy
-        print('Accuracy for fold %d: %d %%' % (fold, 100.0 * correct / total))
+            # Print accuracy
+            print('Accuracy for fold %d: %d %%' % (fold, 100.0 * correct / total))
+            print('--------------------------------')
+            results[fold] = 100.0 * (correct / total)
+            
+        # Print fold results
+        print(f'K-FOLD CROSS VALIDATION RESULTS FOR {num_folds} FOLDS')
         print('--------------------------------')
-        results[fold] = 100.0 * (correct / total)
-        
-    # Print fold results
-    print(f'K-FOLD CROSS VALIDATION RESULTS FOR {num_folds} FOLDS')
-    print('--------------------------------')
-    sum = 0.0
-    for key, value in results.items():
-        print(f'Fold {key}: {value} %')
-        sum += value
-    print(f'Average: {sum/len(results.items())} %')
+        sum = 0.0
+        for key, value in results.items():
+            print(f'Fold {key}: {value} %')
+            sum += value
+        print(f'Average: {sum/len(results.items())} %')
     
 
 if __name__ == '__main__':
