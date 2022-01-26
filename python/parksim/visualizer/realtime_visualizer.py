c201a829e1b816a79296b8be3b600d1580f4e175
XuShenLZ <xushen0118@outlook.com>
2022-01-25 19:31:59 -0800

    add draw_text and grid

--- python/parksim/visualizer/realtime_visualizer.py
+++ python/parksim/visualizer/realtime_visualizer.py
@@ -1,4 +1,6 @@
+from tracemalloc import start
 from typing import List
+from matplotlib.pyplot import yticks
 import numpy as np
 import dearpygui.dearpygui as dpg
 
@@ -27,10 +29,11 @@ class RealtimeVisualizer(object):
         self.map_height = self.dlpvis.map_size['y'] * 10
 
         # self.canvas = dpg.add_window(width=self.map_width, height=self.map_height, label="Map")
-        self.map_window = dpg.add_window(width=self.map_width, height=self.map_height, label="Map")
+        self.map_window = dpg.add_window(width=self.map_width+50, height=self.map_height+50, label="Map")
 
         self.scene_canvas = dpg.add_draw_layer(parent=self.map_window)
         self.frame_canvas = dpg.add_draw_layer(parent=self.map_window)
+        self.grid_canvas = dpg.add_draw_layer(parent=self.map_window)
 
         with dpg.theme() as win_theme:
             with dpg.theme_component(dpg.mvAll):
@@ -41,11 +44,24 @@ class RealtimeVisualizer(object):
         # Plot lines and static obstacles
         self.draw_scene(scene_token=dataset.list_scenes()[0])
 
+        # Control Window
+        self.control_window = dpg.add_window(width=100, height=200, pos=[self.map_width+50, 0], label="Simulation Control", autosize=True)
+        
+        self.start_pause_btn = dpg.add_button(label="Start/Pause", callback=self.running_cb, parent=self.control_window)
+
+        # Running Status
+        self.running = False
+        self.running_display = dpg.add_text(default_value="Runing: " + str(self.running), parent=self.control_window)
+
         dpg.setup_dearpygui()
         dpg.show_viewport()
 
     def __del__(self):
         dpg.destroy_context()
+
+    def running_cb(self):
+        self.running = not self.running
+        dpg.set_value(self.running_display, "Running: " + str(self.running))
 
     def _xy2p(self, x, y):
         """
@@ -90,6 +106,24 @@ class RealtimeVisualizer(object):
 
             dpg.draw_quad(p1=[px[0], py[0]], p2=[px[1], py[1]], p3=[px[2], py[2]], p4=[px[3], py[3]], fill=(0,0,255,255), color=(0,0,0,0), parent=self.scene_canvas)
 
+    def _draw_grids(self, interval:int =10):
+        xticks = range(0, self.dlpvis.map_size['x'], interval)
+        yticks = range(0, self.dlpvis.map_size['y'], interval)
+
+        for x in xticks:
+            p1 = self._xy2p(x, 0)
+            p2 = self._xy2p(x, self.dlpvis.map_size['y'])
+            dpg.draw_line(p1=p1, p2=p2, color=(0,0,0, 40), parent=self.grid_canvas)
+            dpg.draw_text([p1[0]+2, p1[1]+2], str(x), color=(0,0,0,255), size=15, parent=self.grid_canvas)
+
+        for y in yticks:
+            p1 = self._xy2p(0, y)
+            p2 = self._xy2p(self.dlpvis.map_size['x'], y)
+
+            dpg.draw_line(p1=p1, p2=p2, color=(0,0,0, 40), parent=self.grid_canvas)
+            dpg.draw_text([2, p1[1]+2], str(y), color=(0,0,0,255), size=15, parent=self.grid_canvas)
+
+
     def _gen_vehicle_corners(self, state):
         """
         state: array-like with size (3,) for x, y, psi
@@ -121,6 +155,19 @@ class RealtimeVisualizer(object):
 
         dpg.draw_polyline(points=poli_xy, color=color, parent=self.frame_canvas)
 
+    def draw_text(self, position, text: str, size: float=15, color: tuple =(0,0,0,255) ):
+        """
+        Adds text to the figure
+        
+        position: an array-like object with size 2. It is the (x,y) position of the top left point of bounding text rectangle.
+        text: text string
+        size: text size
+        tuple: color tuple
+        """
+        px, py = self._xy2p(position[0], position[1])
+
+        dpg.draw_text(pos=[px, py], text=text, color=color, size=size, parent=self.frame_canvas)
+
     def draw_scene(self, scene_token):
         """
         plot lines and static obstacles in a specified scene
@@ -133,6 +180,8 @@ class RealtimeVisualizer(object):
 
         # Plot static obstacles
         self._draw_obstacles(scene_token)
+
+        self._draw_grids()
 
     def clear_frame(self):
         dpg.delete_item(self.frame_canvas, children_only=True)
